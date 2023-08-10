1a46201197b997edacaf2d99c79a653b082c14ff
sy_work <95818270@qq.com>
2023-08-10 16:46:02 +0800

    Signed-off-by: sy_work <95818270@qq.com>

--- pyprint/print1.py
+++ pyprint/print1.py
@@ -1,7 +1,10 @@
+# coding: utf-8" 
+
 import qrcode
 from PIL import Image
 import win32print
 import win32ui
+import win32con
 from PIL import ImageWin
 
 
@@ -23,13 +26,47 @@ if __name__ == '__main__':
     hDC = win32ui.CreateDC()
     hDC.CreatePrinterDC(printer_name)
 
+    # 获取当前打印机纸张大小
+    page_size = hDC.GetDeviceCaps(
+        win32con.PHYSICALWIDTH), hDC.GetDeviceCaps(win32con.PHYSICALHEIGHT)
+    print('page size: {} x {}'.format(page_size[0], page_size[1]))
+
     bmp = Image.open('qr_code.png')
-    # if bmp.size[0] > bmp.size[1]:
-    #     bmp = bmp.rotate(90)
     hDC.StartDoc('qr_code')
     hDC.StartPage()
+
+    hDC.MoveTo(0, 100)
+    hDC.LineTo(page_size[0], 100)
+    
+    font = win32ui.CreateFont({
+        # 'name': 'Arial',
+        'height': 100,
+    })
+    hDC.SelectObject(font)
+    hDC.TextOut(350, 0, '开始的内容说明')
+
+
+    font = win32ui.CreateFont({
+        # 'name': 'Arial',
+        'height': 90,
+    })
+    hDC.SelectObject(font)
+    hDC.TextOut(350, 150, '第二行的内容说明第二行的内容说明第第二行')
+
+
+    font = win32ui.CreateFont({
+        # 'name': 'Arial',
+        'height': 50,
+
+    })
+    hDC.SelectObject(font)
+    hDC.TextOut(350, 250, '第三行的内容说明')
+
+
+
     dib = ImageWin.Dib(bmp)
-    dib.draw(hDC.GetHandleOutput(), (0, 0, 1000, 1000))
+    # 参数是一个元组，表示绘制的位置和大小。元组中的四个数字分别代表左上角的x坐标、左上角的y坐标、右下角的x坐标和右下角的y坐标
+    dib.draw(hDC.GetHandleOutput(), (0, 0, bmp.size[0], bmp.size[1]))
 
     hDC.EndPage()
     hDC.EndDoc()
