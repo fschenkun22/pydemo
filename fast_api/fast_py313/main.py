ca9c3dfb127fa250c407aa126db7e00325e67f44
sy_work <95818270@qq.com>
2025-05-15 17:53:29 +0800

    测试提交

---
+++ fast_api/fast_py313/main.py
@@ -0,0 +1,26 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    print("Hello World")
+    return {"Hello": "World111"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+
+# 主程序
+if __name__ == "__main__":
+    import uvicorn
+
+    # 使用导入路径字符串而不是应用实例
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+
